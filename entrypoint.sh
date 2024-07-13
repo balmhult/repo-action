af1210668cd0f10d3e4d2a28364ee5d785f62176
Toko <t@quantum.tech>
2024-07-13 12:02:31 +0900

    run everyday

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,218 +3,201 @@
 set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 
-# Time and day check
-current_hour=$(date +"%H")
-current_day=$(date +"%u")
+echo "[+] Action start"
+SOURCE_BEFORE_DIRECTORY="${1}"
+SOURCE_DIRECTORY="${2}"
+DESTINATION_GITHUB_USERNAME="${3}"
+DESTINATION_REPOSITORY_NAME="${4}"
+GITHUB_SERVER="${5}"
+USER_EMAIL="${6}"
+USER_NAME="${7}"
+DESTINATION_REPOSITORY_USERNAME="${8}"
+TARGET_BRANCH="${9}"
+COMMIT_MESSAGE="${10}"
+TARGET_DIRECTORY="${11}"
+CREATE_TARGET_BRANCH_IF_NEEDED="${12}"
 
-# JST timezone offset
-JST_hour_offset=9
-current_hour_JST=$(( (current_hour + JST_hour_offset) % 24 ))
+if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
+then
+	DESTINATION_REPOSITORY_USERNAME="$DESTINATION_GITHUB_USERNAME"
+fi
 
-if [ "$current_day" -ge 1 ] && [ "$current_day" -le 5 ]; then
-  if [ "$current_hour_JST" -ge 9 ] && [ "$current_hour_JST" -lt 18 ]; then
-    echo "[+] Action start"
+if [ -z "$USER_NAME" ]
+then
+	USER_NAME="$DESTINATION_GITHUB_USERNAME"
+fi
 
-    SOURCE_BEFORE_DIRECTORY="${1}"
-    SOURCE_DIRECTORY="${2}"
-    DESTINATION_GITHUB_USERNAME="${3}"
-    DESTINATION_REPOSITORY_NAME="${4}"
-    GITHUB_SERVER="${5}"
-    USER_EMAIL="${6}"
-    USER_NAME="${7}"
-    DESTINATION_REPOSITORY_USERNAME="${8}"
-    TARGET_BRANCH="${9}"
-    COMMIT_MESSAGE="${10}"
-    TARGET_DIRECTORY="${11}"
-    CREATE_TARGET_BRANCH_IF_NEEDED="${12}"
+# Verify that there (potentially) some access to the destination repository
+# and set up git (with GIT_CMD variable) and GIT_CMD_REPOSITORY
+if [ -n "${SSH_DEPLOY_KEY:=}" ]
+then
+	echo "[+] Using SSH_DEPLOY_KEY"
 
-    if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
-    then
-      DESTINATION_REPOSITORY_USERNAME="$DESTINATION_GITHUB_USERNAME"
-    fi
+	# Inspired by https://github.com/leigholiver/commit-with-deploy-key/blob/main/entrypoint.sh , thanks!
+	mkdir --parents "$HOME/.ssh"
+	DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
+	echo "${SSH_DEPLOY_KEY}" > "$DEPLOY_KEY_FILE"
+	chmod 600 "$DEPLOY_KEY_FILE"
 
-    if [ -z "$USER_NAME" ]
-    then
-      USER_NAME="$DESTINATION_GITHUB_USERNAME"
-    fi
+	SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
+	ssh-keyscan -H "$GITHUB_SERVER" > "$SSH_KNOWN_HOSTS_FILE"
 
-    # Verify that there (potentially) some access to the destination repository
-    # and set up git (with GIT_CMD variable) and GIT_CMD_REPOSITORY
-    if [ -n "${SSH_DEPLOY_KEY:=}" ]
-    then
-      echo "[+] Using SSH_DEPLOY_KEY"
+	export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
 
-      # Inspired by https://github.com/leigholiver/commit-with-deploy-key/blob/main/entrypoint.sh , thanks!
-      mkdir --parents "$HOME/.ssh"
-      DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
-      echo "${SSH_DEPLOY_KEY}" > "$DEPLOY_KEY_FILE"
-      chmod 600 "$DEPLOY_KEY_FILE"
+	GIT_CMD_REPOSITORY="git@$GITHUB_SERVER:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
 
-      SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
-      ssh-keyscan -H "$GITHUB_SERVER" > "$SSH_KNOWN_HOSTS_FILE"
+elif [ -n "${API_TOKEN_GITHUB:=}" ]
+then
+	echo "[+] Using API_TOKEN_GITHUB"
+	GIT_CMD_REPOSITORY="https://$DESTINATION_REPOSITORY_USERNAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
+else
+	echo "::error::API_TOKEN_GITHUB and SSH_DEPLOY_KEY are empty. Please fill one (recommended the SSH_DEPLOY_KEY)"
+	exit 1
+fi
 
-      export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
+CLONE_DIR=$(mktemp -d)
 
-      GIT_CMD_REPOSITORY="git@$GITHUB_SERVER:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
+echo "[+] Git version"
+git --version
 
-    elif [ -n "${API_TOKEN_GITHUB:=}" ]
-    then
-      echo "[+] Using API_TOKEN_GITHUB"
-      GIT_CMD_REPOSITORY="https://$DESTINATION_REPOSITORY_USERNAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
-    else
-      echo "::error::API_TOKEN_GITHUB and SSH_DEPLOY_KEY are empty. Please fill one (recommended the SSH_DEPLOY_KEY)"
-      exit 1
-    fi
+echo "[+] Enable git lfs"
+git lfs install
 
-    CLONE_DIR=$(mktemp -d)
+echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
 
-    echo "[+] Git version"
-    git --version
+# Setup git
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
 
-    echo "[+] Enable git lfs"
-    git lfs install
+# workaround for https://github.com/cpina/github-action-push-to-another-repository/issues/103
+git config --global http.version HTTP/1.1
 
-    echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
-
-    # Setup git
-    git config --global user.email "$USER_EMAIL"
-    git config --global user.name "$USER_NAME"
-
-    # workaround for https://github.com/cpina/github-action-push-to-another-repository/issues/103
-    git config --global http.version HTTP/1.1
-
-    {
-      git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
-    } || {
-        if [ "$CREATE_TARGET_BRANCH_IF_NEEDED" = "true" ]
-        then
-            # Default branch of the repository is cloned. Later on the required branch
-      # will be created
-            git clone --single-branch --depth 1 "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
-        else
-            false
-        fi
-    } || {
-      echo "::error::Could not clone the destination repository. Command:"
-      echo "::error::git clone --single-branch --branch $TARGET_BRANCH $GIT_CMD_REPOSITORY $CLONE_DIR"
-      echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
-      echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB OR SSH_DEPLOY_KEY"
-      exit 1
-    }
-    ls -la "$CLONE_DIR"
-
-    TEMP_DIR=$(mktemp -d)
-    # This mv has been the easier way to be able to remove files that were there
-    # but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
-    # including "." and with the exception of ".git/"
-    mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
-
-    # $TARGET_DIRECTORY is '' by default
-    ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
-
-    echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
-    rm -rf "$ABSOLUTE_TARGET_DIRECTORY"
-
-    echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
-    mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
-
-    echo "[+] Listing Current Directory Location"
-    ls -al
-
-    echo "[+] Listing root Location"
-    ls -al /
-
-    mv "$TEMP_DIR/.git" "$CLONE_DIR/.git"
-
-    echo "[+] List contents of $SOURCE_DIRECTORY"
-    ls "$SOURCE_DIRECTORY"
-
-    echo "[+] Checking if local $SOURCE_DIRECTORY exist"
-    if [ ! -d "$SOURCE_DIRECTORY" ]
-    then
-      echo "ERROR: $SOURCE_DIRECTORY does not exist"
-      echo "This directory needs to exist when push-to-another-repository is executed"
-      echo
-      echo "In the example it is created by ./build.sh: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L19"
-      echo
-      echo "If you want to copy a directory that exist in the source repository"
-      echo "to the target repository: you need to clone the source repository"
-      echo "in a previous step in the same build section. For example using"
-      echo "actions/checkout@v2. See: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L16"
-      exit 1
-    fi
-
-    echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
-    cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
-    cd "$CLONE_DIR"
-
-       echo "[+] Files that will be pushed"
-    ls -la
-
-    ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-    COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
-    COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
-
-    echo "[+] Set directory is safe ($CLONE_DIR)"
-    # Related to https://github.com/cpina/github-action-push-to-another-repository/issues/64
-    git config --global --add safe.directory "$CLONE_DIR"
-
+{
+	git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
+} || {
     if [ "$CREATE_TARGET_BRANCH_IF_NEEDED" = "true" ]
     then
-        echo "[+] Switch to the TARGET_BRANCH"
-        # || true: if the $TARGET_BRANCH already existed in the destination repo:
-        # it is already the current branch and it cannot be switched to
-        # (it's not needed)
-        # If the branch did not exist: it switches (creating) the branch
-        git switch -c "$TARGET_BRANCH" || true
-    fi
-
-    # Function to generate a random time between the last hour and now
-    generate_random_time() {
-        local START_TIME=$(date --date='1 hour ago' +%s)
-        local END_TIME=$(date +%s)
-        local RANDOM_TIME=$((START_TIME + RANDOM % (END_TIME - START_TIME)))
-        date --date="@$RANDOM_TIME" '+%Y-%m-%d %H:%M:%S'
-    }
-
-    STATE_FILE="$CLONE_DIR/commit_state.txt"
-    if [ -f "$STATE_FILE" ]; then
-        LAST_PROCESSED_COMMIT=$(cat "$STATE_FILE")
+        # Default branch of the repository is cloned. Later on the required branch
+	# will be created
+        git clone --single-branch --depth 1 "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
     else
-        LAST_PROCESSED_COMMIT=""
+        false
     fi
+} || {
+	echo "::error::Could not clone the destination repository. Command:"
+	echo "::error::git clone --single-branch --branch $TARGET_BRANCH $GIT_CMD_REPOSITORY $CLONE_DIR"
+	echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
+	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB OR SSH_DEPLOY_KEY"
+	exit 1
+}
+ls -la "$CLONE_DIR"
 
-    COMMITS_TO_PROCESS=$(cd "$SOURCE_DIRECTORY" && git log --format="%H" --reverse --no-merges "$LAST_PROCESSED_COMMIT"..HEAD | head -n 3)
+TEMP_DIR=$(mktemp -d)
+# This mv has been the easier way to be able to remove files that were there
+# but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
+# including "." and with the exception of ".git/"
+mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
 
-    if [ -z "$COMMITS_TO_PROCESS" ]; then
-        echo "No new commits to process."
-        exit 0
-    fi
+# $TARGET_DIRECTORY is '' by default
+ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
 
-    echo "[+] Adding git commit"
-    # Cherry-pick the commits one by one with adjusted commit times
-    for COMMIT in $COMMITS_TO_PROCESS; do
-        RANDOM_COMMIT_DATE=$(generate_random_time)
-        GIT_COMMITTER_DATE="$RANDOM_COMMIT_DATE" git cherry-pick $COMMIT --no-commit
-        GIT_AUTHOR_DATE="$RANDOM_COMMIT_DATE" git commit --no-edit --amend --allow-empty
-        echo $COMMIT > "$STATE_FILE"
-    done
+echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
+rm -rf "$ABSOLUTE_TARGET_DIRECTORY"
 
-    echo "[+] git status:"
-    git status
+echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
+mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
 
-    echo "[+] git diff-index:"
-    # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
-    git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+echo "[+] Listing Current Directory Location"
+ls -al
 
-    echo "[+] Pushing git commit"
-    # --set-upstream: sets the branch when pushing to a branch that does not exist
-    git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+echo "[+] Listing root Location"
+ls -al /
 
-    echo "[+] Action end"
-  else
-    echo "[+] Outside of working hours. Exiting."
-  fi
-else
-  echo "[+] Outside of working days. Exiting."
+mv "$TEMP_DIR/.git" "$CLONE_DIR/.git"
+
+echo "[+] List contents of $SOURCE_DIRECTORY"
+ls "$SOURCE_DIRECTORY"
+
+echo "[+] Checking if local $SOURCE_DIRECTORY exist"
+if [ ! -d "$SOURCE_DIRECTORY" ]
+then
+	echo "ERROR: $SOURCE_DIRECTORY does not exist"
+	echo "This directory needs to exist when push-to-another-repository is executed"
+	echo
+	echo "In the example it is created by ./build.sh: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L19"
+	echo
+	echo "If you want to copy a directory that exist in the source repository"
+	echo "to the target repository: you need to clone the source repository"
+	echo "in a previous step in the same build section. For example using"
+	echo "actions/checkout@v2. See: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L16"
+	exit 1
 fi
+
+echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
+cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
+cd "$CLONE_DIR"
+
+echo "[+] Files that will be pushed"
+ls -la
+
+ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
+
+echo "[+] Set directory is safe ($CLONE_DIR)"
+# Related to https://github.com/cpina/github-action-push-to-another-repository/issues/64
+git config --global --add safe.directory "$CLONE_DIR"
+
+if [ "$CREATE_TARGET_BRANCH_IF_NEEDED" = "true" ]
+then
+    echo "[+] Switch to the TARGET_BRANCH"
+    # || true: if the $TARGET_BRANCH already existed in the destination repo:
+    # it is already the current branch and it cannot be switched to
+    # (it's not needed)
+    # If the branch did not exist: it switches (creating) the branch
+    git switch -c "$TARGET_BRANCH" || true
+fi
+
+# Function to generate a random time between the last hour and now
+generate_random_time() {
+    local START_TIME=$(date --date='1 hour ago' +%s)
+    local END_TIME=$(date +%s)
+    local RANDOM_TIME=$((START_TIME + RANDOM % (END_TIME - START_TIME)))
+    date --date="@$RANDOM_TIME" '+%Y-%m-%d %H:%M:%S'
+}
+
+STATE_FILE="$CLONE_DIR/commit_state.txt"
+if [ -f "$STATE_FILE" ]; then
+    LAST_PROCESSED_COMMIT=$(cat "$STATE_FILE")
+else
+    LAST_PROCESSED_COMMIT=""
+fi
+
+COMMITS_TO_PROCESS=$(cd "$SOURCE_DIRECTORY" && git log --format="%H" --reverse --no-merges "$LAST_PROCESSED_COMMIT"..HEAD | head -n 3)
+
+if [ -z "$COMMITS_TO_PROCESS" ]; then
+    echo "No new commits to process."
+    exit 0
+fi
+
+echo "[+] Adding git commit"
+# Cherry-pick the commits one by one with adjusted commit times
+for COMMIT in $COMMITS_TO_PROCESS; do
+    RANDOM_COMMIT_DATE=$(generate_random_time)
+    GIT_COMMITTER_DATE="$RANDOM_COMMIT_DATE" git cherry-pick $COMMIT --no-commit
+    GIT_AUTHOR_DATE="$RANDOM_COMMIT_DATE" git commit --no-edit --amend --allow-empty
+    echo $COMMIT > "$STATE_FILE"
+done
+
+echo "[+] git status:"
+git status
+
+echo "[+] git diff-index:"
+# git diff-index : to avoid doing the git commit failing if there are no changes to be commit
+git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+
+echo "[+] Pushing git commit"
+# --set-upstream: sets de branch when pushing to a branch that does not exist
+git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+
+echo "[+] Action end"
